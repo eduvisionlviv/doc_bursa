1dbaef552ad11953554c2b7b763622cefa5f682f
Боднар Матвій Ярославович <eduvision.lviv.hd@gmail.com>
2025-12-26 17:21:57 +0200

    Create DashboardView.xaml.cs

---
+++ Views/DashboardView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace FinDesk.Views
+{
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
