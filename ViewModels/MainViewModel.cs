08677d15abbfad7d0bcb1b0683d3700b3f6319d7
Боднар Матвій Ярославович <eduvision.lviv.hd@gmail.com>
2025-12-26 17:19:08 +0200

    Update and rename MainWindowViewModel.cs to MainViewModel.cs

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FinDesk.Views;
+
+namespace FinDesk.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private object? currentView;
+
+        public MainViewModel()
+        {
+            CurrentView = new DashboardView();
+        }
+
+        [RelayCommand]
+        private void Navigate(string viewName)
+        {
+            CurrentView = viewName switch
+            {
+                "Dashboard" => new DashboardView(),
+                "Transactions" => new TransactionsView(),
+                "Sources" => new SourcesView(),
+                _ => CurrentView
+            };
+        }
+    }
+}
