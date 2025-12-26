0002b7bbb0a35c19386c8e65730f112632418f0d
Боднар Матвій Ярославович <eduvision.lviv.hd@gmail.com>
2025-12-26 21:11:14 +0200

    Add GroupsView user control with ViewModel

---
+++ Views/GroupsView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+using System.Windows.Controls;
+using doc_bursa.ViewModels;
+
+namespace doc_bursa.Views
+{
+    public partial class GroupsView : UserControl
+    {
+        public GroupsView()
+        {
+            InitializeComponent();
+            DataContext = new GroupsViewModel();
+        }
+    }
+}
