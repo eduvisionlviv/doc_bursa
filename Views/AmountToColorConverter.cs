2073c0445d6efad38145a4caf676e04c7efb718b
Боднар Матвій Ярославович <eduvision.lviv.hd@gmail.com>
2025-12-26 17:23:10 +0200

    Add AmountToColorConverter for color conversion
    Implement a value converter to convert decimal amounts to colors.

---
+++ Views/AmountToColorConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FinDesk.Converters
+{
+    public class AmountToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal amount)
+            {
+                return amount >= 0 
+                    ? new SolidColorBrush(Color.FromRgb(39, 174, 96))
+                    : new SolidColorBrush(Color.FromRgb(231, 76, 60));
+            }
+
+            return new SolidColorBrush(Colors.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
