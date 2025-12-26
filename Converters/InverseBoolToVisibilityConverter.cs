4dd7835dcf2b9c80eed3f30bd43577e45dd2e143
Боднар Матвій Ярославович <eduvision.lviv.hd@gmail.com>
2025-12-26 18:38:38 +0200

    Add InverseBoolToVisibilityConverter class

---
+++ Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+InverseBoolToVisibilityConverter.csusing System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FinDesk.Converters
+{
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
