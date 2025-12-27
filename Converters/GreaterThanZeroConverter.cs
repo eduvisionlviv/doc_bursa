2415a43787ff190221342d7ecf6037a71ae11b50
Боднар Матвій Ярославович <eduvision.lviv.hd@gmail.com>
2025-12-27 14:02:36 +0200

    Add GreaterThanZeroConverter for value checks
    - Add value converter for checking if number is greater than zero
    - Support decimal, double, int, and long types
    - Used for coloring transactions (green for positive, red for negative)
    - Includes XML documentation in Ukrainian

---
+++ Converters/GreaterThanZeroConverter.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace doc_bursa.Converters
+{
+    /// <summary>
+    /// Конвертер для перевірки чи значення більше нуля
+    /// Використовується для кольорування транзакцій (зелений/червоний)
+    /// </summary>
+    public class GreaterThanZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal decimalValue)
+                return decimalValue > 0;
+            
+            if (value is double doubleValue)
+                return doubleValue > 0;
+            
+            if (value is int intValue)
+                return intValue > 0;
+            
+            if (value is long longValue)
+                return longValue > 0;
+            
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Метод ConvertBack не підтримується для GreaterThanZeroConverter");
+        }
+    }
+}
